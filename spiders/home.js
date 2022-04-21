1c0b13f93d614b01d6209686d7f404966be030fa
Jose <joserodrigues.globant@latam.com>
2022-04-21 16:09:49 -0400

    Init

---
+++ spiders/home.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+const urls = ["https://www.bancoestado.cl/imagenes/_personas/home/default.asp?desk"];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  for (let i = 0; i < urls.length; i++) {
+    const url = urls[i];
+    try{
+      await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
+      await page.goto(`${url}`);
+      await page.screenshot({ path: `./screenshots/home/home${i}.png` });
+    } catch (error){
+      console.error(`this ${url} could not be scrapped, error ${error}`)
+    }
+  }
+  await browser.close();
+})();
